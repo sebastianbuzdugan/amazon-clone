8a3d4c94da292c3c2650133af0bc064d5de02770
sebastianbuzdugan <81054605+sebastianbuzdugan@users.noreply.github.com>
2021-07-13 15:23:44 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,86 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Header from "./Header";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Orders from "./Orders";
+
+
+const promise = loadStripe(
+  'pk_test_51JCMOoLCNCgoIG1Mbr5Diz0tTnjZd0I5ADY2mWrYBBgUSJNO7df3bvxz4fMQ3JZFndHbnwOznRJFaaiJZdKq2mds0030cfqHCE'
+  );
+
+
+function App() {
+
+const [{}, dispatch] = useStateValue();
+
+useEffect(() => {
+  auth.onAuthStateChanged(authUser => {
+    console.log("THE USER IS >>>", authUser);
+
+    if (authUser){
+dispatch({
+  type: 'SET_USER',
+  user: authUser
+})
+    } else {
+      dispatch({
+        type: 'SET_USER',
+        user: null
+      })
+
+    }
+  })
+}, [])
+
+return ( 
+<Router>
+  <div className="app">
+      <Switch>
+      <Route path="/orders">
+      <Header />
+         <Orders />
+         </Route>
+      <Route path="/login">
+         
+         <Login />
+         </Route>
+
+        <Route path="/checkout">
+        <Header />
+          <Checkout />
+          </Route>
+
+          <Route path="/payment">
+        <Header />
+
+        <Elements stripe={promise}>
+          <Payment />
+
+        </Elements>
+  
+          </Route>
+
+          <Route path="/">
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
+
+   </div>
+</Router>
+  
+);
+
+}
+
+  export default App
